c6ec63d4033a8854821230d25a179a823ab50911
invokeImmediately <danielcrieck@gmail.com>
2023-09-10 21:20:19 -0700

    Fix: Tree ADT → .toString; +ES6 syntax, Δstyle
    Update «WsMapper.tree.js» with fixes. First, rename the method «WsMapp
     er.Tree.printStr» to «toString» to better reflect its purpose. Then
     simplify the format of the output by dropping the right arrow char-
     acters that were previously used so that only box art characters are
     employed to signify the hierarchical structure of the tree. Next, up-
     date the overall syntax utilized in the module to ES6. Finally, up-
     date the code indentation style to a tighter tab length of 2 and
     space characters.

--- WsMapper.tree.js
+++ WsMapper.tree.js
@@ -1,290 +1,257 @@
-/*!*************************************************************************************************
- * WsMapper.tree.js
- * -------------------------------------------------------------------------------------------------
- * SUMMARY: Implementation of a tree ADT for use on the Website Mapper project.
+/*!*****************************************************************************
+ * ▓ WsMapper.▐▀█▀▌█▀▀▄ █▀▀▀ █▀▀▀  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▓▓▓
+ * ▓▓ ▒▒▒▒▒▒▒▒  █  █▄▄▀ █▀▀  █▀▀    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▓▓
+ * ▓▓▓ ▒▒▒▒▒▒▒▒ █  ▀  ▀▄▀▀▀▀ ▀▀▀▀.js ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▓
  *
- * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ * Implementation of a tree ADT for use on the Website Mapper project.
  *
- * REPOSITORY: https://github.com/invokeImmediately/WSU-UE---JS
+ * @version 0.0.0
  *
- * LICENSE: MIT License
+ * @author: Daniel Rieck
+ *  [daniel.rieck@wsu.edu]
+ *  (https://github.com/invokeImmediately)
  *
- * Copyright (c) 2020 Daniel C.Rieck.
+ * @link https://github.com/invokeImmediately/WebsiteMapper
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * @license MIT - Copyright (c) 2023 Daniel C.Rieck.
  *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-**************************************************************************************************/
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions: *
+ * The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software. *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ ******************************************************************************/
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // TABLE OF CONTENTS
 // -----------------
-//   §1: Script dependencies..................................................................43
-//     §1.1: Node.js includes.................................................................46
-//     §1.2: Namespace declaration............................................................53
-//   §2: Class declarations...................................................................58
-//     §2.1: WsMapper.Tree....................................................................61
-//     §2.2: WsMapper.TreeNode...............................................................142
-//   §3: Class testing.......................................................................269
-////////////////////////////////////////////////////////////////////////////////////////////////////
+//   §1: Script dependencies..................................................43
+//     §1.1: Node.js includes.................................................46
+//     §1.2: Namespace declaration............................................53
+//   §2: Class declarations...................................................58
+//     §2.1: WsMapper.Tree....................................................61
+//     §2.2: WsMapper.TreeNode...............................................142
+//   §3: Class testing.......................................................269
+////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+"use strict";
+
+////////////////////////////////////////////////////////////////////////////////
 // §1: Script dependencies
 
 ////////
 // §1.1: Node.js includes
 
-var tty = require('tty');
-var ttys = require('ttys');
-var stdout = ttys.stdout;
+const tty = require('tty');
+const ttys = require('ttys');
+const stdout = ttys.stdout;
 
 ////////
 // §1.2: Namespace declaration
 
-var WsMapper = WsMapper || {};
+const WsMapper = {};
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // §2: Class declarations
 
 ////////
 // §2.1: WsMapper.Tree
 
-/**
- * @todo Add inline documentation.
- * @todo Finish writing class.
- */
 WsMapper.Tree = class Tree {
-	/**
-	 * @todo Add inline documentation.
-	 */
-	constructor( rootValue ) {
-		this.root = new WsMapper.TreeNode(
-			rootValue,
-			0,
-			0,
-			undefined,
-			this
-		);
-		this.treeType = typeof rootValue;
-		this.lastAdded = this.root;
-	}
 
-	/**
-	 * @todo Add inline documentation.
-	 */
-	findFirst( value ) {
-		return this.root.findFirst( value );
-	}
+  constructor( rootValue ) {
+    this.root = new WsMapper.TreeNode(
+      rootValue,
+      0,
+      0,
+      undefined,
+      this
+    );
+    this.treeType = typeof rootValue;
+    this.lastAdded = this.root;
+  }
 
-	/**
-	 * @todo Add inline documentation.
-	 * @todo Finish writing function.
-	 */
-	getPrintStr() {
-		var curIdx = [0];
-		var curDepth = 0
-		var curNode = this.root.children[curIdx];
-		var outHdr = '';
-		var outStr = outHdr + this.root.value + '\n';
-		var counter = 0;
+  findFirst( value ) {
+    return this.root.findFirst( value );
+  }
 
-		curIdx.push( 0 );
-		while ( curNode !== undefined && counter < 100 ) {
-			outHdr = outHdr.replace( '├─→', '│  ' );
-			outHdr = outHdr.replace( '└─→', '   ' );
-			if ( curNode.getSibNext() === undefined ) {
-				outHdr += '└─→ ';
-			} else {
-				outHdr += '├─→ ';				
-			}
-			outStr += outHdr + curNode.value + '\n';
-			if ( curNode.children.length ) {
-				curNode = curNode.children[0];
-			} else if ( curNode.getSibNext() !== undefined ) {
-				curNode = curNode.getSibNext();
-				outHdr = outHdr.slice(0, outHdr.length - 4 );
-			} else {
-				curNode = curNode.parent;
-				outHdr = outHdr.slice(0, outHdr.length - 4 );
-				while (
-					curNode !== undefined &&
-					curNode.getSibNext() === undefined &&
-					counter < 100
-				) {
-					curNode = curNode.parent;
-					outHdr = outHdr.slice(0, outHdr.length - 4 );
-					counter++;
-				}
-				if ( curNode !== undefined ) {
-					curNode = curNode.getSibNext();
-					outHdr = outHdr.slice(0, outHdr.length - 4 );
-				}
-			}
-			counter++;
-		}
+  toString() {
+    let curDepth = 0
+    let curNode = this.root.children[ 0 ];
+    let outHdr = '';
+    let outStr = outHdr + this.root.value + '\n';
+    let counter = 0;
 
-		return outStr;
-	}
+    while ( curNode !== undefined && counter < 100 ) {
+      outHdr = outHdr.replace( '├─', '│ ' );
+      outHdr = outHdr.replace( '└─', '  ' );
+      if ( curNode.getSibNext() === undefined ) {
+        outHdr += '└─ ';
+      } else {
+        outHdr += '├─ ';
+      }
+      outStr += outHdr + curNode.value + '\n';
+      if ( curNode.children.length ) {
+        curNode = curNode.children[0];
+      } else if ( curNode.getSibNext() !== undefined ) {
+        curNode = curNode.getSibNext();
+        outHdr = outHdr.slice(0, outHdr.length - 3 );
+      } else {
+        curNode = curNode.parent;
+        outHdr = outHdr.slice(0, outHdr.length - 3 );
+        while (
+          curNode !== undefined &&
+          curNode.getSibNext() === undefined &&
+          counter < 100
+        ) {
+          curNode = curNode.parent;
+          outHdr = outHdr.slice(0, outHdr.length - 3 );
+          counter++;
+        }
+        if ( curNode !== undefined ) {
+          curNode = curNode.getSibNext();
+          outHdr = outHdr.slice(0, outHdr.length - 3 );
+        }
+      }
+      counter++;
+    }
+
+    return outStr;
+  }
 }
 
 ////////
 // §2.2: WsMapper.TreeNode
 
-/**
- * @todo Add inline documentation.
- * @todo Finish writing class.
- */
 WsMapper.TreeNode = class TreeNode {
-	/**
-	 * @todo Add inline documentation.
-	 */
-	constructor( value, idx, depth, parentNode, parentTree ) {
-		this.value = value;
-		this.idx = idx;
-		this.depth = depth;
-		this.parent = parentNode;
-		this.tree = parentTree
-		this.children = [];
-	}
 
-	/**
-	 * Append a new node to this node's children.
-	 * 
-	 * @todo Finish inline documentation.
-	 */
-	addChild( value ) {
-		var newNode = new TreeNode(
-			value,
-			this.children.length,
-			this.depth + 1,
-			this,
-			this.tree
-		);
-		this.children.push( newNode );
-		this.tree.lastAdded = newNode;
+  constructor( value, idx, depth, parentNode, parentTree ) {
+    this.value = value;
+    this.idx = idx;
+    this.depth = depth;
+    this.parent = parentNode;
+    this.tree = parentTree
+    this.children = [];
+  }
 
-		return newNode;
-	}
+  addChild( value ) {
+    const newNode = new TreeNode(
+      value,
+      this.children.length,
+      this.depth + 1,
+      this,
+      this.tree
+    );
+    this.children.push( newNode );
+    this.tree.lastAdded = newNode;
 
-	/**
-	 * @todo Append a new node to this node's siblings.
-	 */
-	addSib( value ) {
-		var newNode = undefined;
+    return newNode;
+  }
 
-		if ( this.parent !== undefined ) {
-			newNode = new TreeNode(
-				value,
-				this.parent.children.length,
-				this.depth,
-				this.parent,
-				this.tree
-			);
-			this.parent.children.push( newNode );
-			this.tree.lastAdded = newNode;
-		}
+  addSib( value ) {
+    let newNode = undefined;
 
-		return newNode;
-	}
+    if ( this.parent !== undefined ) {
+      newNode = new TreeNode(
+        value,
+        this.parent.children.length,
+        this.depth,
+        this.parent,
+        this.tree
+      );
+      this.parent.children.push( newNode );
+      this.tree.lastAdded = newNode;
+    }
 
-	/**
-	 * @todo Add inline documentation.
-	 */
-	findFirst( value ) {
-		var result = undefined;
+    return newNode;
+  }
 
-		if ( value == this.value ) {
-			result = this;
-		} else if ( this.children.length ) {
-			for ( var jdx = 0; !result && jdx < this.children.length; jdx++ ) {
-				result = this.children[ jdx ].findFirst( value );
-			}
-		}
+  findFirst( value ) {
+    let result = undefined;
 
-		return result;
-	}
+    if ( value == this.value ) {
+      result = this;
+    } else if ( this.children.length ) {
+      for ( let jdx = 0; !result && jdx < this.children.length; jdx++ ) {
+        result = this.children[ jdx ].findFirst( value );
+      }
+    }
 
-	/**
-	 * @todo Add inline documentation.
-	 */
-	getPathToRoot() {
-		var path = [];
-		var curNode = this.parent;
+    return result;
+  }
 
-		path.splice( 0, 0, this.idx );
-		for ( var jdx = this.depth - 1; jdx > 0; jdx-- ) {
-			path.splice( 0, 0, curNode.idx);
-			curNode = curNode.parent;
-		}
+  getPathToRoot() {
+    const path = [];
+    let curNode = this.parent;
 
-		return path;
-	}
+    path.splice( 0, 0, this.idx );
+    for ( let jdx = this.depth - 1; jdx > 0; jdx-- ) {
+      path.splice( 0, 0, curNode.idx);
+      curNode = curNode.parent;
+    }
 
-	/**
-	 * @todo Add inline documentation.
-	 */
-	getSibNext() {
-		var next;
+    return path;
+  }
 
-		if (
-			this.parent === undefined ||
-			this.idx >= this.parent.children.length - 1
-		) {
-			next = undefined; 
-		} else {
-			next = this.parent.children[ this.idx + 1 ];
-		}
+  getSibNext() {
+    var next;
 
-		return next;
-	}
+    if (
+      this.parent === undefined ||
+      this.idx >= this.parent.children.length - 1
+    ) {
+      next = undefined;
+    } else {
+      next = this.parent.children[ this.idx + 1 ];
+    }
 
-	/**
-	 * @todo Add inline documentation.
-	 */
-	getSibPrev() {
-		var prev;
+    return next;
+  }
 
-		if ( this.idx == 0 ) {
-			prev = undefined; 
-		} else {
-			prev = this.parent.children[ this.idx - 1 ];
-		}
+  getSibPrev() {
+    var prev;
 
-		return prev;
-	}
+    if ( this.idx == 0 ) {
+      prev = undefined;
+    } else {
+      prev = this.parent.children[ this.idx - 1 ];
+    }
+
+    return prev;
+  }
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // §3: Class testing
 
 WsMapper.testTrees = function () {
-	var tree = new WsMapper.Tree( 'Node 1' );
-	var node;
-	var node2;
-	var foundNode;
+  let tree = new WsMapper.Tree( 'Node 1' );
+  let node;
+  let node2;
+  let foundNode;
 
-	tree.root.addChild( 'Node 2' );
-	node = tree.root.addChild( 'Node 3' );
-	node.addChild( 'Node 4' );
-	node2 = node.addChild( 'Node 5' );
-	node.addChild( 'Node 6' );
-	node.addSib( 'Node 7' );
-	tree.root.addSib( 'Node 8' );
-	node2.addChild( 'Node 9' );
-	node2.addChild( 'Node 10' );
+  tree.root.addChild( 'Node 2' );
+  node = tree.root.addChild( 'Node 3' );
+  node.addChild( 'Node 4' );
+  node2 = node.addChild( 'Node 5' );
+  node.addChild( 'Node 6' );
+  node.addSib( 'Node 7' );
+  tree.root.addSib( 'Node 8' );
+  node2.addChild( 'Node 9' );
+  let node3 = node2.addChild( 'Node 10' );
 
-	console.log( tree.getPrintStr() );
+  console.log( tree.toString() );
+  console.log( node3.getPathToRoot() );
 }
 
 WsMapper.testTrees();
